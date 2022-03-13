28e0ea9aea46ac7971dc19547564c735a99ae1cd
Jatin Kumar <jackkr121@gmail.com>
2022-03-13 14:45:10 +0530

    fixed advertisement slider

--- index.php
+++ index.php
@@ -15,10 +15,23 @@
 <!--animated-css-->
 <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
 <script src="js/wow.min.js"></script>
+<!--/animated-css-->
+<!-- requried-jsfiles-for owl -->
+<link href="css/owl.carousel.css" rel="stylesheet">
+<script src="js/owl.carousel.js"></script>
 <script>
  new WOW().init();
+ $(document).ready(function() {
+	$("#owl-demo").owlCarousel({
+		items : 1,
+		lazyLoad : true,
+		autoPlay : true,
+		navigation : false,
+		navigationText :  false,
+		pagination : true,
+	});
+});
 </script>
-<!--/animated-css-->
 </head>
 <body>
 <!--header-->
@@ -172,49 +185,26 @@
 		  </div>
 	  </div>
 	  <div class="container">
-		<!-- requried-jsfiles-for owl -->
-				<link href="css/owl.carousel.css" rel="stylesheet">
-							    <script src="js/owl.carousel.js"></script>
-							        <script>
-							    $(document).ready(function() {
-							      $("#owl-demo").owlCarousel({
-							        items : 1,
-							        lazyLoad : true,
-							        autoPlay : true,
-							        navigation : false,
-							        navigationText :  false,
-							        pagination : true,
-							      });
-							    });
-							    </script>
-			<!-- //requried-jsfiles-for owl -->
-		 <div id="owl-demo" class="owl-carousel">
-         
-         
-         
-         <?php
+		 <div id="owl-demo" class="owl-carousel">         
+<?php
 $s="select * from advertisement";
 $result=mysqli_query($cn,$s);
 
-$n=0;
-while($data=mysqli_fetch_array($result))
-{
+$n = 0;
+while($data=mysqli_fetch_array($result)) {
 	if($n%4==0)
 	{
 	?>
          
-			  <div class="item text-center guide-sliders">
-<?php }?>
-
-
+<div class="item text-center guide-sliders">
+<?php } ?>
 	<div class="col-md-3 image-grid">
-					 <img src="Admin/addverimages/<?php echo $data[3]; ?>" width="300px" height="150px" >
-					
-					 <p><a href="#"><?php echo $data[2]; ?></a></p>
-				 </div>
-				<?php if($n%4==3)
-				{?>
-            </div>
+		<img src="Admin/addverimages/<?php echo $data[3]; ?>">
+		<small><a href="#"><?php echo $data[2]; ?></a></small>
+	</div>
+<?php if($n%4==3) {
+?>
+</div>
    
             <?php
 				}
